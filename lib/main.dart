dc96241c9629873d205eeef0a4cf6b2a0d8ccc5b
PanjiGunawan090 <panjigunawan090@gmail.com>
2022-03-16 02:01:40 +0700

    Panji Tampan

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+mport 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+      Center(
+        child: Text(
+          'Halo dunia!',
+          textDirection: TextDirection.ltr,
+        ),
+      )
+  );
+}
